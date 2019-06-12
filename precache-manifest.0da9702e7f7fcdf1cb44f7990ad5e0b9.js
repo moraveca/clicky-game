c9e9da479b72f003d0e31de1685c3eb5bb06c87e
arm <moraveca@gmail.com>
2019-06-11 22:19:52 -0500

    Updates

---
+++ precache-manifest.0da9702e7f7fcdf1cb44f7990ad5e0b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a5d57e52a9b5f826b3b117b4aeb8da",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "850a82d1cc14cd1cba19",
+    "url": "/clicky-game/static/css/main.cd9ea37f.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "850a82d1cc14cd1cba19",
+    "url": "/clicky-game/static/js/main.b5df5d7f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
